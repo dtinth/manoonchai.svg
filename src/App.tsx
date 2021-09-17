75f4cea588437c64ad7275cf24593275eb37677b
Thai Pangsakulyanont <dtinth@spacet.me>
2021-09-17 15:44:29 +0000

    Add settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,71 @@
 import './App.css'
-import React from 'react'
+import React, { useState } from 'react'
 import Keyboard, { Settings, settingsSchema } from './Keyboard'
 
 export default function App() {
-  const settings = useSettings()
+  const [settings, changeSetting] = useSettings()
   return (
     <div className="App">
       <Keyboard settings={settings} />
+      <p>
+        {Object.entries(settingsSchema).map(
+          ([key, { options, default: defaultValue }]) => {
+            const id = `settings-${key}`
+            return (
+              <span key={key}>
+                <label htmlFor={id}>{key}</label>
+                <select
+                  id={id}
+                  value={(settings as any)[key]}
+                  onChange={(e) => changeSetting(key, e.target.value)}
+                >
+                  {Object.entries(options).map(([value, label]) => (
+                    <option key={value} value={value}>
+                      {label}
+                    </option>
+                  ))}
+                </select>
+              </span>
+            )
+          },
+        )}
+      </p>
     </div>
   )
 }
 
-function useSettings(): Settings {
-  return Object.fromEntries(
-    Object.entries(settingsSchema).map(([key, { default: defaultValue }]) => {
-      return [key, defaultValue]
-    }),
-  ) as any
+function useSettings(): [Settings, (key: string, value: string) => void] {
+  const [settings, setSettings] = useState<Settings>(
+    () =>
+      Object.fromEntries(
+        Object.entries(settingsSchema).map(
+          ([key, { options, default: defaultValue }]) => {
+            const localStorageKey = getLocalStorageKey(key)
+            const valueFromLocalStorage = localStorage.getItem(localStorageKey)
+            const value =
+              valueFromLocalStorage && (options as any)[valueFromLocalStorage]
+                ? valueFromLocalStorage
+                : defaultValue
+            return [key, value]
+          },
+        ),
+      ) as any,
+  )
+
+  const changeSetting = (key: string, value: string) => {
+    const localStorageKey = getLocalStorageKey(key)
+    localStorage.setItem(localStorageKey, value)
+    setSettings((oldSettings) => {
+      return {
+        ...oldSettings,
+        [key]: value,
+      }
+    })
+  }
+
+  return [settings, changeSetting]
+
+  function getLocalStorageKey(key: string) {
+    return `dtinth.layoutvisualizer.${key}`
+  }
 }
