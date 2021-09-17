30e5c6e316923bf83d40235364fb555e6cfdf25e
Thai Pangsakulyanont <dtinth@spacet.me>
2021-09-17 16:44:08 +0000

    Fix font rendering

--- api/_render.tsx
+++ api/_render.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Keyboard from '../src/Keyboard'
 import fs from 'fs'
 
-const waree = fs.readFileSync(require.resolve('../src/fonts/waree.css'))
+const waree = fs.readFileSync(require.resolve('../src/fonts/waree.css'), 'utf8')
 
 export function renderSvg(settings: any) {
   return <Keyboard settings={settings} extra={<style>{waree}</style>} />
